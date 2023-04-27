85635c567387737a15f62ef2a21c172cb2051d0e
sjtg <chipilej@gmail.com>
2023-04-27 11:40:24 +0200

    Removal of the secret key

--- grant_tracker/grant_tracker/settings.py
+++ grant_tracker/grant_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)pz7pc5(2zymz#4)2@m4=sbue2)tati2f5k+y8wif)@n857djp"
+SECRET_KEY = "secret key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
