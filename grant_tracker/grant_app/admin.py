d488a242596bb9e11dba2a52d394ecb9e80100c4
sjtg <chipilej@gmail.com>
2023-04-28 17:54:52 +0200

    Registered Category and Priority in admin.py

--- grant_tracker/grant_app/admin.py
+++ grant_tracker/grant_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Grant_Management
+from .models import Category, Priority, Grant_Management
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Priority)
 admin.site.register(Grant_Management)
